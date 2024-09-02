221cc0ef3807f9ea9dc1b606cf61bdfbc4d0056e
Anna Yakimova <petrova-omck@mail.ru>
2024-09-02 14:33:26 +0600

    Начинаем программировать_1

--- webpack.config.js
+++ webpack.config.js
@@ -14,4 +14,18 @@ const path = require('path');
        patterns: [{ from: 'public' }],
      }),
    ],
+   module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          },
+        },
+      },
+    ]
+  }
  };
